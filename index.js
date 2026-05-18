00a28247c9c5e290ae8fd53c4e1977b78a3f21bb
Junndrop <143789698+Junndrop@users.noreply.github.com>
2026-05-18 09:22:28 +0700

    Refactor testAPI function for improved error handling

--- index.js
+++ index.js
@@ -2,38 +2,41 @@ const axios = require("axios");
 
 const API_KEY = process.env.API_KEY;
 
-async function testAPI() {
+async function testAPI(){
 
-    try {
+try{
 
-        console.log("START TEST");
+console.log("KEY ADA:", !!API_KEY);
+console.log("PANJANG:", API_KEY.length);
 
-        const res = await axios.get(
-            "https://www.eldorado.gg/api/orders/me",
-            {
-                headers:{
-                    "X-API-KEY": API_KEY,
-                    "accept":"application/json"
-                }
-            }
-        );
+const res = await axios.get(
+"https://www.eldorado.gg/api/orders/me",
+{
+headers:{
+"Authorization":"Bearer " + API_KEY,
+"accept":"application/json"
+}
+}
+);
 
-        console.log("SUCCESS");
-        console.log(res.data);
+console.log("SUCCESS");
+console.log(res.data);
 
-    } catch(err){
+}catch(err){
 
-        console.log("ERROR");
+console.log("STATUS:",
+err.response?.status);
 
-        console.log(
-            err.response?.status
-        );
+console.log("DATA:");
+console.log(
+JSON.stringify(
+err.response?.data,
+null,
+2
+)
+);
 
-        console.log(
-            err.response?.data || err.message
-        );
-
-    }
+}
 
 }
 
