866ec4154b0c14ae84182956cb85cf991ed0c322
Junndrop <143789698+Junndrop@users.noreply.github.com>
2026-05-18 12:05:36 +0700

    Fix userPoolClientId typo in index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ Amplify.configure({
 Auth:{
 Cognito:{
 userPoolId:"us-east-2_MlnzCFGhK",
-userPoolClientId:"1956req5ro9drdtbf5i6kis4la",
+userPoolClientId:"1956req5ro9drdtbf5i6ksi4la",
 loginWith:{
 email:true
 }
